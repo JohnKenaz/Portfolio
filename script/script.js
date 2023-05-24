9e63f9ca972d50001e6d2589059e665319f14309
John Kenaz Pena <johnkenaz.pena@gsc.edu.ph>
2023-05-24 16:51:28 +0800

    I have make changes on date and time and remove some designs. Then I decided to add new contact page.

--- script/script.js
+++ script/script.js
@@ -1,39 +1,40 @@
 // TYPEWRITER EFFECT
-      
-    const text = "WELCOME TO MY PORTFOLIO";
-      let index = 0;
+const text = "WELCOME TO MY PORTFOLIO";
+let index = 0;
 
-      function typeWriter() {
-        if (index < text.length) {
-          document.getElementById("typewriter").innerHTML += text.charAt(index);
-          index++;
-          setTimeout(typeWriter, Math.floor(Math.random() * 100) + 50); // use variable delay of 50-250ms
-        }
-        else {
-          setTimeout(function() {
-            document.getElementById("typewriter").innerHTML = ""; // Clear the current text
-            index = 0; // Reset the index variable to 0 to repeat typing
-            typeWriter(); // Start typing again
-          }, 3000); // Wait for 3 second before starting again
-        }
-      }
-      typeWriter();
-  
-//TIME AND DATE
+function typeWriter() {
+  if (index < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, Math.floor(Math.random() * 100) + 50); // use variable delay of 50-250ms
+  }
+  else {
+    setTimeout(function() {
+      document.getElementById("typewriter").innerHTML = ""; // Clear the current text
+      index = 0; // Reset the index variable to 0 to repeat typing
+      typeWriter(); // Start typing again
+    }, 3000); // Wait for 3 second before starting again
+  }
+}
+typeWriter();
 
+//DATE AND TIME
+// create a function to update the date and time
+function updateDateTime() {
     // create a new Date object
     let currentDate = new Date();
-
-    // set the timezone to "America/New_York"
+  
+    // set the timezone to "Asia/Manila"
     let options = { timeZone: 'Asia/Manila' };
-
+  
     // format the date and time as a string with the specified timezone
     let dateStr = currentDate.toLocaleDateString('en-US', options);
     let timeStr = currentDate.toLocaleTimeString('en-US', options);
-
+  
     // display the date and time on the web page
     document.getElementById("date").innerHTML = dateStr;
     document.getElementById("time").innerHTML = timeStr;
-
-    // update the date and time display every second
-    setInterval(updateDateTime, 1000);
+  }
+  
+  // call the updateDateTime function every second using setInterval()
+  setInterval(updateDateTime, 1000);
