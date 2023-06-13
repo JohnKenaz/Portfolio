0a1648a58a879379c9696c19b6582e780f27d77c
John Kenaz Pena <johnkenaz.pena@gsc.edu.ph>
2023-06-13 15:53:13 +0800

    Changed the contact design.

--- script/script.js
+++ script/script.js
@@ -1,22 +1,21 @@
-// TYPEWRITER EFFECT
 const text = "WELCOME TO MY PORTFOLIO";
-let index = 0;
+      let index = 0;
 
-function typeWriter() {
-  if (index < text.length) {
-    document.getElementById("typewriter").innerHTML += text.charAt(index);
-    index++;
-    setTimeout(typeWriter, Math.floor(Math.random() * 100) + 50); // use variable delay of 50-250ms
-  }
-  else {
-    setTimeout(function() {
-      document.getElementById("typewriter").innerHTML = ""; // Clear the current text
-      index = 0; // Reset the index variable to 0 to repeat typing
-      typeWriter(); // Start typing again
-    }, 3000); // Wait for 3 second before starting again
-  }
-}
-typeWriter();
+      function typeWriter() {
+        if (index < text.length) {
+          document.getElementById("typewriter").innerHTML += text.charAt(index);
+          index++;
+          setTimeout(typeWriter, Math.floor(Math.random() * 100) + 50); // use variable delay of 50-250ms
+        }
+        else {
+          setTimeout(function() {
+            document.getElementById("typewriter").innerHTML = ""; // Clear the current text
+            index = 0; // Reset the index variable to 0 to repeat typing
+            typeWriter(); // Start typing again
+          }, 3000); // Wait for 3 second before starting again
+        }
+      }
+      typeWriter();
 
 //DATE AND TIME
 // create a function to update the date and time
